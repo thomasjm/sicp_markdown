eae574349ea4c4350aea9b8b1810647a271e96a2
Tom McLaughlin <pyro777@gmail.com>
2017-09-14 17:10:29 -0700

    Improve math spacing and lost code formatting in exercises

--- process.sh
+++ process.sh
@@ -19,7 +19,7 @@ sed -i 's/{.lisp/{.scheme/g' $OUT
 sed -i 's/ .prettyprinted style=""//g' $OUT
 
 # Make sure block quotes have a space for empty lines
-sed -i 's/^>/> /g' $OUT
+sed -i 's/^>$/> /g' $OUT
 
 # Remove div lines
 perl -i -pe 's/<\/div>\n//g' $OUT
@@ -45,6 +45,11 @@ perl -i -pe 's/^#\s+/# /g' $OUT
 perl -i -pe 's/^##\s+/## /g' $OUT
 perl -i -pe 's/^###\s+/### /g' $OUT
 
+# Make exercise blocks not indented
+# Do this before moving MathJax block maths to their own lines, because that can break up an exercise
+# Do this before removing extra space block quotes, because that would deindent the exercises
+python deindent_exercises.py $OUT $OUT
+
 # Remove extra spaces in bullets and block quotes
 perl -i -pe 's/^-\s+/- /g' $OUT
 perl -i -pe 's/^>\s+/> /g' $OUT
@@ -57,10 +62,21 @@ perl -i -pe 's/\[\^(\d+)\^\]\([^\)]*\)\{[^}]*\}/\[\^$1\]/g' $OUT
 # Turn footnotes into inline footnotes
 python inliner.py $OUT $OUT
 
-# Make exercise blocks not indented
-# Do this before moving MathJax block maths to their own lines, because that can break
-# up an exercise
-python deindent_exercises.py $OUT $OUT
-
 # Make sure MathJax block maths are on their own lines
-perl -i -pe 'BEGIN {undef $/;} s/(\$\$[^\$]+\$\$)/\n$1\n/sgm' $OUT
+# perl -i -pe 'BEGIN {undef $/;} s/(\$\$[^\$]+\$\$)/\n\n$1\n\n/sgm' $OUT
+perl -i -pe 'BEGIN {undef $/;} s/(\$\$[^\$]+\$\$)/\n\n$1\n\n/sgm' $OUT
+# We might have over-spaced, some, so fix this
+# Remove extra newlines at the end
+perl -i -pe 'BEGIN {undef $/;} s/(\$\$[^\$]+\$\$)\n\n\n\n/$1\n\n/sgm' $OUT
+perl -i -pe 'BEGIN {undef $/;} s/(\$\$[^\$]+\$\$)\n\n\n/$1\n\n/sgm' $OUT
+# Remove extra newlines at the beginning
+perl -i -pe 'BEGIN {undef $/;} s/\n\n\n\n(\$\$[^\$]+\$\$)/\n\n$1/sgm' $OUT
+perl -i -pe 'BEGIN {undef $/;} s/\n\n\n(\$\$[^\$]+\$\$)/\n\n$1/sgm' $OUT
+
+# Remove the footnotes heading at the bottom
+perl -i -pe 's/## Footnotes//g' $OUT
+
+# Remove extra newlines at end of file
+# https://unix.stackexchange.com/questions/81685/how-to-remove-multiple-newlines-at-eof
+awk '/^$/ {nlstack=nlstack "\n";next;} {printf "%s",nlstack; nlstack=""; print;}' $OUT > tmp.md
+cp tmp.md $OUT
